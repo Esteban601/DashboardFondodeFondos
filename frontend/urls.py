be9e96fb4f5dc7ba824185e18064c5d95d20d542
yoidel86 <yoidel86@gmail.com>
2023-04-05 15:57:16 -0400

    datos kpis sociles

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,20 +5,6 @@ from . import views
 urlpatterns = [
         # path('', views.index, name='index'),
         path('', views.ingresos, name='ingresos'),
-        path('materiales', views.materiales, name='materiales'),
-        path('energia', views.energia, name='energia'),
-        path('agua-afluentes', views.agua_afluentes, name='agua_afluentes'),
-        path('emisiones', views.emisiones, name='emisiones'),
-        path('residuos', views.residuos, name='residuos'),
-        path('etica_anticorrupcion', views.etica_anticorrupcion, name='etica_anticorrupcion'),
-        path('gobierno_corporativo', views.gobierno_corporativo, name='gobierno_corporativo'),
-        path('analisis_riesgo', views.analisis_riesgo, name='analisis_riesgo'),
-        path('productos_calidad', views.productos_calidad, name='productos_calidad'),
-        path('salud_seguridad', views.salud_seguridad, name='salud_seguridad'),
-        path('capacitacion', views.capacitacion, name='capacitacion'),
-        path('equidad_genero', views.equidad_genero, name='equidad_genero'),
-        path('relacion_clientes', views.relacion_clientes, name='relacion_clientes'),
-        path('ciberseguridad', views.ciberseguridad, name='ciberseguridad'),
 
         path('ingresos', views.ingresos, name='ingresos'),
         path('costos', views.costos, name='costos'),
@@ -43,7 +29,8 @@ urlpatterns = [
         path('reduccion_consumo_energia', views.reduccion_consumo_energia, name='reduccion_consumo_energia'),
 
         path('empleados_funcion', views.empleados_funcion, name='empleados_funcion'),
-        path('empleados_sindicato', views.empleados_sindicato, name='empleados_sindicato'),
+        path('satisfaccion_empleados', views.satisfaccion_empleados, name='satisfaccion_empleados'),
+        path('rotacion_empleados', views.rotacion_empleados, name='rotacion_empleados'),
 
         path('accionistas_mayoritarios', views.accionistas_mayoritarios, name='accionistas_mayoritarios'),
         path('consejo_admon', views.consejo_admon, name='consejo_admon'),
--- frontend/views.py
+++ frontend/views.py
@@ -11,117 +11,6 @@ def index(request):
     return render(request, 'frontend/index.html', context)
 
 
-def materiales(request):
-    context = {
-        'page': _('materiales'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/materiales.html', context)
-
-
-def energia(request):
-    context = {
-        'page': _('energía'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/energia.html', context)
-
-
-def agua_afluentes(request):
-    context = {
-        'page': _('agua y afluentes'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/agua_afluentes.html', context)
-
-
-def emisiones(request):
-    context = {
-        'page': _('emisiones'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/emisiones.html', context)
-
-
-def residuos(request):
-    context = {
-        'page': _('residuos'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/residuos.html', context)
-
-
-def analisis_riesgo(request):
-    context = {
-        'page': _('Análisis de riesgos (climático y sistémico)'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/analisis_riesgo.html', context)
-
-
-def etica_anticorrupcion(request):
-    context = {
-        'page': _('etica y anticorrupcion'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/etica_anticorrupcion.html', context)
-
-
-def gobierno_corporativo(request):
-    context = {
-        'page': _('gobierno corporativo'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/gobierno_corporativo.html', context)
-
-
-def productos_calidad(request):
-    context = {
-        'page': _('productos de calidad'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/productos_calidad.html', context)
-
-
-def salud_seguridad(request):
-    context = {
-        'page': _('salud y seguridad de los empleados'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/salud_seguridad.html', context)
-
-
-def capacitacion(request):
-    context = {
-        'page': _('capacitación'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/capacitacion.html', context)
-
-
-def equidad_genero(request):
-    context = {
-        'page': _('equidad de género y diversidad'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/equidad_genero.html', context)
-
-
-def relacion_clientes(request):
-    context = {
-        'page': _('relación con clientes y comunidades'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/relacion_clientes.html', context)
-
-
-def ciberseguridad(request):
-    context = {
-        'page': _('ciberseguridad'),
-        'section':'medio ambiente'
-    }
-    return render(request, 'frontend/ciberseguridad.html', context)
-
 ##################
 # Económicos
 ##################
@@ -305,12 +194,20 @@ def empleados_funcion(request):
     return render(request, 'frontend/sociales/empleados_funcion.html', context)
 
 
-def empleados_sindicato(request):
+def satisfaccion_empleados(request):
     context = {
-        'page': _('empleados_sindicato'),
+        'page': _('satisfaccion_empleados'),
         'section': _('sociales')
     }
-    return render(request, 'frontend/sociales/empleados_sindicato.html', context)
+    return render(request, 'frontend/sociales/satisfaccion_empleados.html', context)
+
+
+def rotacion_empleados(request):
+    context = {
+        'page': _('rotacion_empleados'),
+        'section': _('sociales')
+    }
+    return render(request, 'frontend/sociales/rotacion_empleados.html', context)
 
 #############################
 #gobierno_corporativo
